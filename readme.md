74024efa5882007a2d8d3f485e475ea6ccee7c20
Supraofc <lhannasupra@gmail.com>
2024-01-16 23:08:54 -0300

    SupraOfc

---
+++ readme.md
@@ -0,0 +1,30 @@
+# SupraOfc - Bot de Sinais para Telegram
+
+## Como Usar:
+
+1. **Clone o Repositório:**
+   ```bash
+   git clone https://github.com/SupraOfc/telegram-bot.git
+   
+Instale as Dependências:
+Certifique-se de ter o Node.js instalado. Em seguida, navegue até o diretório do projeto e execute o seguinte comando:
+
+npm install
+Configure suas Credenciais:
+Abra o arquivo tiger.js e substitua os valores de api_key, chat_id, e LINK_SITE pelas suas próprias credenciais.
+
+Inicie o Bot:
+Execute o comando abaixo para iniciar o bot:
+
+bash
+
+node tiger.js
+Aproveite os Sinais:
+O bot enviará automaticamente sinais com oportunidades imperdíveis. Certifique-se de que o bot está em execução para receber notificações.
+
+Observações:
+O bot envia sinais automaticamente a cada minuto.
+Uma notificação especial será enviada a cada 4 minutos.
+Certifique-se de configurar corretamente as credenciais para evitar erros.
+Criador:
+Este bot foi desenvolvido por SupraOfc. Siga-o no GitHub para mais projetos interessantes.
---
+++ tiger.js
@@ -0,0 +1,82 @@
+const { Telegraf } = require('telegraf');
+
+const api_key = '6835953720:AAEHM7GtpJbxRmWt1p1t0hPgYFUMJnXMoA8';
+const chat_id = '-1002001279046';
+const LINK_SITE = 'https://goldbet.games/cadastro?refId=Mjc1N3JlZklk';
+const bot = new Telegraf(api_key);
+
+
+async function startBot() {
+    let iterationCount = 0;
+
+    setInterval(async () => {
+        iterationCount++;
+
+        const now = new Date();
+        const tz = 'America/Sao_Paulo';
+        const timeNow = new Date(now.toLocaleString('en-US', { timeZone: tz }));
+
+        let h = timeNow.getHours();
+        let m = timeNow.getMinutes();
+        let s = timeNow.getSeconds();
+
+        if (m > 59) {
+            h += 1;
+            m = 0;
+        }
+        if (h === 9) {
+            m = `0${h}`;
+        }
+        if (m < 9) {
+            m = `0${m}`;
+        }
+        if (s < 9) {
+            s = `0${s}`;
+        }
+        console.log(`${h}:${m}:${s}`);
+
+        const numero_aleatorio1 = Math.floor(Math.random() * 10) + 1;
+        const numero_aleatorio2 = Math.floor(Math.random() * 10) + 1;
+
+        console.log(numero_aleatorio1, numero_aleatorio2);
+
+     await bot.telegram.sendPhoto(
+        chat_id,
+        { source: 'tigrinho.jpg' },
+        {
+            caption: `
+🐯 *Sinais do Mestre - Oportunidade Imperdível!* 🚀
+
+🎲 *FORTUNE TIGER - Nova Oportunidade!*
+- ⏰ *Tolerância:* 3 minutos
+- 🎯 *Gire:* ${numero_aleatorio2}X no *TURBO* e recarregue o jogo
+- 🔄 Depois, gire ${numero_aleatorio1}X *MANUAL*
+- 🟢 *Saia no Primeiro Grande Ganho!*
+
+🌐 *Plataforma dos Sinais:* [Clique Aqui](${LINK_SITE})
+🔗 *Sinais funcionam apenas na plataforma acima* 
+
+📢 *Não perca essa chance! Chame seus amigos e jogue agora!*
+
+🤑 *Boa sorte e que venham os ganhos!* 🤑
+            `,
+            parse_mode: 'Markdown',
+            disable_web_page_preview: true
+        }
+    );
+
+    }, 60000);
+
+    setTimeout(async () => {
+        await bot.telegram.sendMessage(
+            chat_id,
+            `🎯 *Boas notícias!* 🌟\n\nA *CARTA LIBERADA* \n\n *GREEN*! 🟢💰`,
+            {
+                parse_mode: 'Markdown',
+                disable_web_page_preview: true
+            }
+        );
+    }, 240000);
+}
+
+startBot();
